82f7d0bd03b6ddf4e4ef58879377b8ed1f4bbffa
Administrator <Administrator@WIN.w>
2022-10-05 03:38:16 -0700

    test commit

--- BashNeft.CSCource2022.TestAppCore/Program.cs
+++ BashNeft.CSCource2022.TestAppCore/Program.cs
@@ -8,7 +8,7 @@ namespace BashNeft.CSCource2022.TestAppCore
          public static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);   
         static void Main(string[] args)
         {
-          
+        //--  gggggg
         MessageBox(IntPtr.Zero, "Hello World!", "Hello Dialog", 0);
         Console.WriteLine("Hello, World!");
         }
