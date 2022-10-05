c3ba553c9dfb56d5f25db10f9ff9ed061b972e56
Administrator <Administrator@WIN->
2022-10-05 03:33:38 -0700

    Add project files.

---
+++ BashNeft.CSCource2022.TestAppCore/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace BashNeft.CSCource2022.TestAppCore
+{
+    internal class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+         public static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);   
+        static void Main(string[] args)
+        {
+          
+        MessageBox(IntPtr.Zero, "Hello World!", "Hello Dialog", 0);
+        Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ BashNeft.CSCource2022.TestAppFr/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashNeft.CSCource2022.TestAppFr
+{
+
+    internal class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);
+
+        static void Main(string[] args)
+        {
+            MessageBox(IntPtr.Zero, "Hello World!", "Hello Dialog", 0);
+        }
+    }
+}
